2ef92c97923b461991aa4736621a7c3ddd55ced4
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-04-04 20:55:34 +0400

    Completed benefits on index view

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -7,137 +7,43 @@ import Card from '@mui/material/Card';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 
-const mock = [
+import CodeIcon from '@mui/icons-material/Code';
+import InterpreterModeIcon from '@mui/icons-material/InterpreterMode';
+import BiotechIcon from '@mui/icons-material/Biotech';
+import CommitIcon from '@mui/icons-material/Commit';
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
+import GroupsIcon from '@mui/icons-material/Groups';
+
+const benefits = [
   {
-    title: 'Themeable',
-    subtitle:
-      'Customize any part of our components to match your design needs.',
-    icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
-        />
-      </svg>
-    ),
+    title: 'Clean Code',
+    subtitle: 'Following the best SOLID code design principles',
+    icon: <CodeIcon />,
   },
   {
-    title: 'Light and dark UI',
-    subtitle:
-      'Optimized for multiple color modes. Use light or dark, your choice.',
-    icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-        />
-      </svg>
-    ),
+    title: 'Documented Code',
+    subtitle: 'Well documented and self describing semantic code',
+    icon: <InterpreterModeIcon />,
   },
   {
-    title: 'Composable',
-    subtitle:
-      'Designed with composition in mind. Compose new components with ease.',
-    icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"
-        />
-      </svg>
-    ),
+    title: 'Well Tested',
+    subtitle: 'Using test driven development to ensure application integrity',
+    icon: <BiotechIcon />,
   },
   {
-    title: 'Developer experience',
-    subtitle:
-      'Guaranteed to boost your productivity when building your app or website.',
-    icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M13 10V3L4 14h7v7l9-11h-7z"
-        />
-      </svg>
-    ),
+    title: 'Version Control',
+    subtitle: 'Leveraging the latest in Git version control to keep up to date',
+    icon: <CommitIcon />,
   },
   {
-    title: 'Continuous updates',
-    subtitle: 'We continually deploy improvements and new updates to theFront.',
-    icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-        />
-      </svg>
-    ),
+    title: 'Continuous Development',
+    subtitle: 'Continuos development and integration software life cycles',
+    icon: <ChangeCircleIcon />,
   },
   {
-    title: 'Free support',
-    subtitle:
-      '6 months of free technical support to help you build your website faster.',
-    icon: (
-      <svg
-        height={24}
-        width={24}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-        />
-      </svg>
-    ),
+    title: 'Agile Methods',
+    subtitle: 'Harness Agile methodologies to integrate team development',
+    icon: <GroupsIcon />,
   },
 ];
 
@@ -145,7 +51,7 @@ const Benefits = (): JSX.Element => {
   const theme = useTheme();
   return (
     <Grid container spacing={4}>
-      {mock.map((item, i) => (
+      {benefits.map((item, i) => (
         <Grid item xs={12} sm={6} md={4} key={i}>
           <Box
             component={Card}
