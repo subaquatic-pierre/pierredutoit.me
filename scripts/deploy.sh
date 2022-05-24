437688c545b68656d6cfe1b6015bd4d10b098052
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-05-24 13:02:56 +0400

    Update stagin script to copy from public build directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ then
   export $(grep -v '^#' .env | xargs)
 fi
 
-aws s3 cp build/ s3://$MAIN_S3_BUCKET_ID/ --recursive
+aws s3 cp public/ s3://$MAIN_S3_BUCKET_ID/ --recursive
 
 aws cloudfront create-invalidation --distribution-id $MAIN_CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
--- scripts/staging-deploy.sh
+++ scripts/staging-deploy.sh
@@ -5,6 +5,6 @@ then
   export $(grep -v '^#' .env | xargs)
 fi
 
-aws s3 cp build/ s3://$STAGING_S3_BUCKET_ID/ --recursive
+aws s3 cp public/ s3://$STAGING_S3_BUCKET_ID/ --recursive
 
 aws cloudfront create-invalidation --distribution-id $STAGING_CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
